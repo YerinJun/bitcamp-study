a9f5d722826c9179777c87fa323d557fcc6d3498
YerinJun <erin2jj@gmail.com>
2021-06-30 12:50:43 +0900

    update

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello {
+    /*
+        | main() 함수           |
+        | 프로그램 실행 시작점  |
+        | 'entry point'라 부름  | 
+    */
+    public static void main(String[] args){
+    System.out.println("Hello, Wrold!");
+    }
+}
